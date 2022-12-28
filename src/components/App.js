d7e19ad79dd271ec3e9a020d13566a2a09179894
Anju Vishwakarma <111855561+0123anjuvish@users.noreply.github.com>
2022-12-28 13:01:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import useInput from "./useInput";
 import SignInForm from "./SignInForm";
 const App = () => {
   //complete the useInput hook
- 
-  const [email ,bindEmail,resetEmail]= useInput("");
-  const [password, bindPassword,resetPassword] = useInput("");
+  const email = useInput("");
+  const password = useInput("");
+
 
   return (
     <div id="main">
       <SignInForm email={email} password={password} />
       <h1>Result</h1>
       <h2 id="email">Email is: {email.value}</h2>
-      <h2 id="password">Password is: {password.value}</h2>
+      <h2 id="password" >Password is: {password.value}</h2>
     </div>
   );
 };
